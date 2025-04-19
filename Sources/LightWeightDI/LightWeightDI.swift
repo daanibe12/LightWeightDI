8b7152010b4e0cde7ea5260c400709b2db46d45c
ebina12 <ebina12jpjp@gmail.com>
2025-04-19 18:11:26 +0900

    update: container

--- Sources/LightWeightDI/LightWeightDI.swift
+++ Sources/LightWeightDI/LightWeightDI.swift
@@ -1,52 +1,84 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-import SwiftUI
+//
+//  AppContainer.swift
+//  Machiawase
+//
+//  Created by ebina on 2025/04/12.
+//
+
 import Swinject
+import SwiftUI
 
-
-public class DependencyResolver {
-    public static let shared = DependencyResolver()
-    private var container: Container = Container()
-    public func initialize() {
-        container = Container()
+class AppContainer {
+  static let appContainer = Container()
+  public static func resolve<T>() -> T {
+    guard let resolveValue = appContainer.resolve(T.self) else {
+      fatalError("can not resolve")
     }
-    public func resolve<T>() -> T {
-        guard let resolveValue = container.resolve(T.self) else {
-            fatalError("can not resolve")
-        }
-        return resolveValue
-    }
-    
-    public func regist<T>(_ type: T.Type, scope: Swinject.ObjectScope = .weak, factory: @escaping (Swinject.Resolver) -> T) {
-        container.register(type, factory: factory).inObjectScope(scope)
-    }
+    return resolveValue
+  }
+  
+  public static func register<T>(_ type: T.Type, factory: @escaping (Swinject.Resolver) -> T) {
+    appContainer.register(type, factory: factory).inObjectScope(.container)
+  }
+  
+  public static func initialize() {
+    appContainer.removeAll()
+  }
 }
+
+
+
+
+class WeakContainer {
+  static let weakContainer = Container()
+  public static func resolve<T>() -> T {
+    guard let resolveValue = weakContainer.resolve(T.self) else {
+      fatalError("can not resolve")
+    }
+    return resolveValue
+  }
+  
+  public static func register<T>(_ type: T.Type, factory: @escaping (Swinject.Resolver) -> T) {
+    weakContainer.register(type, factory: factory).inObjectScope(.weak)
+  }
+  
+  public static func initialize() {
+    weakContainer.removeAll()
+  }
+}
+
+
 
 @propertyWrapper
 public struct Autowired<T> {
-    private var dependency: T
-
-    public init() {
-        dependency = DependencyResolver.shared.resolve()
+  private var dependency: T
+  
+  public init(isApp: Bool = false) {
+    if isApp {
+      dependency = AppContainer.resolve()
+    } else {
+      dependency = WeakContainer.resolve()
     }
-    public var wrappedValue: T {dependency}
+    
+  }
+  public var wrappedValue: T {dependency}
 }
 
-@propertyWrapper 
-public struct AutowiredStateObj<T>: DynamicProperty where T: ObservableObject {
-    @StateObject private var dependency: T
-    public init() {
-        self._dependency = StateObject(wrappedValue: DependencyResolver.shared.resolve())
+@propertyWrapper
+public struct AutowiredForState<T>: DynamicProperty where T: ObservableObject {
+  @StateObject private var dependency: T
+  public init(isApp: Bool = false) {
+    if isApp {
+      self._dependency = StateObject(wrappedValue: AppContainer.resolve())
+    } else {
+      self._dependency = StateObject(wrappedValue: WeakContainer.resolve())
     }
-    public var wrappedValue: T {
-        get { return dependency }
-    }
-    public var projectedValue: ObservedObject<T>.Wrapper {
-        return self.$dependency
-    }
+    
+  }
+  public var wrappedValue: T {
+    get { return dependency }
+  }
+  public var projectedValue: ObservedObject<T>.Wrapper {
+    return self.$dependency
+  }
 }
-
-
-
-
-
